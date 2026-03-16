35dc1746d95923eee7e7af915fd0e202a87345a8
slnishi <93013772+ShaiyaJ@users.noreply.github.com>
2026-03-16 12:03:43 +0000

    added error checking values for the end user to use and refactored the documentation - scanning for terminal size is now also faster

--- diseq.h
+++ diseq.h
@@ -3,6 +3,11 @@
 
 // =====----- DISEQ constants -----===== //
 typedef enum {
+    SUCCESS,
+    FAILURE
+} DSResult;
+
+typedef enum {
     NONE
 } DSKey;
 
@@ -20,30 +25,49 @@ typedef struct {
 
 
 // =====----- DISEQ functions -----===== //
-// TODO: split these docs up into lines and explain input params?
-
 // Setting up and tearing down the library
 //void ds_init();                                     // Initialises library state
 //void ds_deinit();                                   // Deinitialises library state
 
 // Displaying to the terminal
-void ds_execute(char* string);                      // Displays a single string immediately to the terminal
-void ds_executes(char* fst, ...);                   // Displays a set of strings immediately to the terminal
+void ds_execute(                        // Displays a single string immediately to the terminal
+    char* string                        /* Input string */
+);
 
-void ds_queue(char* string);                        // Queues a single string into a buffer to be displayed later
-void ds_queues(char* fst, ...);                     // Queues a set of strings immediately to a buffer to be displayed later
-void ds_display();                                  // Flushes the buffer that ds_queue() and ds_queues() pushed into
+void ds_executes(                       // Displays a set of strings immediately to the terminal
+    char* fst,                          /* First input string */
+    ...                                 /* Subsequent input string(s) */
+);
+
+
+DSResult ds_queue(                      // Queues a single string into a buffer to be displayed later
+    char* string                        /* Input string */
+);
+
+DSResult ds_queues(                     // Queues a set of strings immediately to a buffer to be displayed later
+    char* fst,                          /* First input string */
+    ...                                 /* Subsequent input string(s) */
+);
+
+void ds_display();                      // Flushes the buffer that ds_queue() and ds_queues() pushed into
+
 
 // Getting information about the terminal
-void dsr_get_cursor_pos(int* row, int* col);        // Returns the current cursor position (requires raw mode)
-void dsr_get_terminal_size(int* rows, int* cols);   // Returns the current terminal size (requires raw mode)
+DSResult dsr_get_cursor_pos(            // Returns the current cursor position (requires raw mode)
+    int* row,                           /* Out parameter to the rows */
+    int* col                            /* Out parameter to the columns */
+);
+
+DSResult dsr_get_terminal_size(         // Returns the current terminal size (requires raw mode)
+    int* row,                           /* Out parameter to the rows */
+    int* col                            /* Out parameter to the columns */
+);
 
 // Manipulating terminal state
-void ds_toggle_raw_mode();                          // Toggles the "raw mode" of the terminal
+DSResult ds_toggle_raw_mode();          // Toggles the "raw mode" of the terminal
 
 // Getting input from the user
-DSKeyPress dsr_raw_input();                           // Returns a single character without blocking - can return nothing
-
+DSKeyPress dsr_raw_input();             // Returns a single character without blocking - can return nothing
 
 
 // =====----- DISEQ libc impl -----===== //
@@ -108,102 +132,129 @@ void _ds_executes(char* fst, ...) {
 #define DI_QUEUE_INIT_CAPACITY 256
 #endif
 
-void ds_queue(char* string) {
-    static char* queue = NULL; 
-    static int size = 0;
-    static int capacity = 0;
+static struct {
+    char* data;
+    int size;
+    int capacity;
+} queue = {NULL, 0, 0};
 
-    // Special case for NULL
-    if (string == NULL) {       // TODO: factor out instead of having a special case?
-        fputs(queue, stdout);
-
-        free(queue);
-        queue = NULL;
-
-        size = 0;
-        capacity = 0;
-        return;
-    }
-    
-
+DSResult ds_queue(char* string) {
     // Reallocate memory if appending string takes more than capacity
     int string_size = strlen(string);
 
-    if ( (size + string_size) >= capacity ) {
-        if (capacity == 0)
-            capacity = DI_QUEUE_INIT_CAPACITY;
+    if ( (queue.size + string_size) >= queue.capacity ) {
+        if (queue.capacity == 0)
+            queue.capacity = DI_QUEUE_INIT_CAPACITY;
 
-        char* new_queue = realloc(queue, capacity * DI_QUEUE_GROWTH_FACTOR);
+        char* new_queue = realloc(queue.data, queue.capacity * DI_QUEUE_GROWTH_FACTOR);
         
         if (new_queue == NULL)
-            return;  // TODO: error value?
+            return FAILURE; 
 
-        queue = new_queue;
-        capacity = capacity * DI_QUEUE_GROWTH_FACTOR;
+        queue.data = new_queue;
+        queue.capacity *= DI_QUEUE_GROWTH_FACTOR;
 
-        ds_queue(string);   // Retry with new factor
-        return;
+        return ds_queue(string);   // Retry with new factor
     }
 
     // Concat string
-    strcat(queue, string);
-    size += string_size;
+    strcat(queue.data, string);
+    queue.size += string_size;
+
+    return SUCCESS;
 }
 
 #define ds_queues(...) _ds_queues(__VA_ARGS__, NULL)
-void _ds_queues(char* fst, ...) {
+DSResult _ds_queues(char* fst, ...) {
     va_list args;
 
     va_start(args, fst);
 
     char* current_arg = fst;
+    DSResult current_res;
 
     do {
-        ds_queue(current_arg);
+        current_res = ds_queue(current_arg);
+
+        // Error handling
+        if (current_res == FAILURE)
+            return FAILURE;
+
         current_arg = va_arg(args, char*);
     } while(current_arg != NULL); 
 
     va_end(args);
+
+    return SUCCESS;
 }
 
 void ds_display() {
-    ds_queue(NULL);
+    fputs(queue.data, stdout);
+
+    free(queue.data);
+    queue.data = NULL;
+
+    queue.size = 0;
+    queue.capacity = 0;
 }
+
 
 
 // Terminal state and raw mode functions //
 
-void dsr_get_cursor_pos(int* row, int* col) {
+DSResult dsr_get_cursor_pos(int* row, int* col) {
+    // Scan for position
     fputs(DI_QUERY_CUR_POS, stdout);
-    
+    fflush(stdout);
+
+    /*
     // Read escape output from stdin 
     char* line = NULL;
     size_t size = 0;
 
-    getdelim(&line, &size, 'R', stdin); // FIXME: blocking
+    getdelim(&line, &size, 'R', stdin); 
     line[size] = '\0';
 
     // Get position info and store in out param
-    sscanf(line, "\033[%d;%dR", row, col);
+    int s = sscanf(line, "\033[%d;%dR", row, col);
     free(line);
+    */
+    int s = scanf("\033[%d;%dR", row, col);
 
-    error:
-        return; // TODO: error value?
+    // Error handling
+    if (s == EOF)
+        return FAILURE;
+    return SUCCESS;
 }
 
-void dsr_get_terminal_size(int* rows, int* cols) {
+DSResult dsr_get_terminal_size(int* rows, int* cols) {
+    DSResult res;
+
     // Get the original position to restore after
     int original_rpos = 0;
     int original_cpos = 0;
 
-    dsr_get_cursor_pos(&original_rpos, &original_cpos);
+    res = dsr_get_cursor_pos(&original_rpos, &original_cpos);
+
+    // Error handling
+    if (res == FAILURE)
+        goto error;
 
     // Set the cursor to 999, 999 and read the value
     DI_SET_CUR_POS(999, 999);
-    dsr_get_cursor_pos(rows, cols);
+    res = dsr_get_cursor_pos(rows, cols);
+
+    // Error handling
+    if (res == FAILURE)
+        goto error;
 
     // Restore the old position
     DI_SET_CUR_POS(original_rpos, original_cpos);
+
+    return SUCCESS;
+
+    error:
+        return FAILURE;
 }
 
 // TODO: implement?
@@ -220,11 +271,11 @@ void dsr_get_terminal_size(int* rows, int* cols) {
 //    DSKey ds_raw_input() {
 //    }
 //#else 
-    void ds_toggle_raw_mode() {
+    DSResult ds_toggle_raw_mode() {
         static struct termios raw_term = {0};
         static struct termios cooked_term = {0};
 
-        static bool is_cooked = false;
+        static bool is_cooked = true;           // By default this is true
         static bool terms_initialised = false;
 
         // Setting terminal defaults if this is the first time running the function
@@ -248,17 +299,20 @@ void dsr_get_terminal_size(int* rows, int* cols) {
         // Toggling the terminal mode
         fflush(stdout);
 
-        if (!is_cooked)
+        if (!is_cooked) {
             if (tcsetattr(STDIN_FILENO, TCSANOW, &cooked_term) < 0)
                 goto error;
-        else
+        } else {
             if (tcsetattr(STDIN_FILENO, TCSANOW, &raw_term) < 0)
                 goto error;
+        }
 
         is_cooked = !is_cooked;
 
+        return SUCCESS;
+
         error:
-            return; // TODO: error value?
+            return FAILURE;
     }
 
     DSKeyPress dsr_raw_input() {
